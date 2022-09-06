abe725a521043ff4cdeca55b1c2b55c08742b2f1
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-09-06 21:11:55 +0100

    Fix linters error

--- student.rb
+++ student.rb
@@ -1,7 +1,6 @@
 require './person'
 
 class Student < Person
-
   def initialize(age:, name:, classroom:, parent_permission: true)
     super(age: age, name: name, parent_permission: parent_permission)
 
