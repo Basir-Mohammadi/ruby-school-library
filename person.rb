bd66181f97909a3c2633db6d0eb0fdc1cc7615a9
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-09-06 21:00:44 +0100

    Add class person

---
+++ person.rb
@@ -0,0 +1,23 @@
+
+class Person
+  attr_reader :id
+  attr_accessor :age, :name
+
+  def initialize(age:, name: 'unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..100)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_service?
+    of_age? || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
