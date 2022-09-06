b3114ada6b8ea9d4c8267b336fea30377a60ae60
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-09-06 21:00:55 +0100

    Add class student

---
+++ student.rb
@@ -0,0 +1,20 @@
+require './person'
+
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(age:, name:, classroom:, parent_permission: true)
+    super(age: age, name: name, parent_permission: parent_permission)
+
+    self.classroom = (classroom)
+  end
+
+  def classroom=(new_classroom)
+    @classroom = new_classroom
+    new_classroom.students << self unless new_classroom.students.include?(self)
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
