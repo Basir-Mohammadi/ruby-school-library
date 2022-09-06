be0e5662a9afdbc794cad2449e960e0745b259bf
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-09-06 21:01:09 +0100

    Add class teacher

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require './person'
+
+class Teacher < Person
+  def initialize(age:, name:, specialization:, parent_permission: true)
+    super(age: age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_service?
+    true
+  end
+end
