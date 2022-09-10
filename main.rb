6aea8794c735b53cae6790da8a2d71b1fd98ee60
Basir Mohammadi <basirahmadmohammadi852@gmail.com>
2022-09-10 07:01:33 +0100

    Refactor class main

--- main.rb
+++ main.rb
@@ -1,37 +1,21 @@
-require './student'
-require './decorators'
-require './classroom'
-require './book'
-
-def cli_separator(char = '-', size = 50)
-  puts char * size
-end
+require './factory'
 
 def main
-  forth_classroom = Classroom.new(label: 'FORTH GRADE')
-  student_x = Student.new(age: 9, name: 'JD', classroom: '4th Grade')
-  forth_classroom.add_student(student_x)
-  forth_classroom.students.each { |s| puts s.classroom.label }
-  permission = student_x.can_use_service? ? 'YES' : 'NO'
-  puts "Can student named #{student_x.name} use service? (#{permission})"
+  factory = Factory.new
 
-  cli_separator
-
-  book = Book.new(title: 'Title 1', author: 'Author 1')
-  person_ls = Person.new(age: 17, name: 'Gregory')
-  rental = Rental.new(person: person_ls, book: book)
-  puts "#{rental.date} == #{person_ls.rentals.first.date}"
-  person_ls.rentals.each { |r| puts r.book.title }
-  book.rentals.each { |r| puts r.person.name }
-
-  cli_separator
-
-  person = Person.new(age: 22, name: 'maximilianus')
-  puts "Original name is: #{person.correct_name}"
-  titled_person = CapitalizeDecorator.new(person)
-  puts "After capitalization, name is: #{titled_person.correct_name}"
-  titled_trimmed_person = TrimmerDecorator.new(titled_person)
-  puts "After both capitalization & trim, name is: #{titled_trimmed_person.correct_name}"
+  loop do
+    choice = factory.run
+    case choice
+    when 1..8
+      factory.process(choice)
+    when 9
+      puts 'App quitting gracefully..'
+      break
+    else
+      puts 'INVALID OPTION'
+    end
+    factory.separator
+  end
 end
 
 main
